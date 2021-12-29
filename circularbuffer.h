899d0046adcd8e6c0905a80d60648d8b66fe6ae3
Kristian Kinderlöv <kristian.kinderlov@husqvarnagroup.com>
2021-12-29 11:50:28 +0100

    Change back the copyright year

--- circularbuffer.h
+++ circularbuffer.h
@@ -1,7 +1,7 @@
 /*
  * MIT License (MIT)
  *
- * Copyright (c) 2021 Kristian Kinderlöv
+ * Copyright (c) 2019 Kristian Kinderlöv
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
